faf4f31a82ee966a4c06b126de5e94f9304620ad
wutarnow <79676753+wutarnow@users.noreply.github.com>
2022-05-09 17:25:18 +0800

    Update cacti.sh

--- cacti-mariadb/cacti.sh
+++ cacti-mariadb/cacti.sh
@@ -4,6 +4,6 @@
 mysql -u root -proot -e "CREATE DATABASE cacti;"
 mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -proot mysql
 mysql -u root -proot -e "ALTER DATABASE cacti CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-mysql -u root -proot -e "GRANT ALL ON cacti.* TO cactiuser@localhost IDENTIFIED BY 'cactiuser';"
-mysql -u root -proot -e "GRANT SELECT ON mysql.time_zone_name TO cactiuser@localhost;"
+mysql -u root -proot -e "GRANT ALL ON cacti.* TO cactiuser IDENTIFIED BY 'cactiuser';"
+mysql -u root -proot -e "GRANT SELECT ON mysql.time_zone_name TO cactiuser;"
 mysql -u root -proot -e "FLUSH privileges;"
