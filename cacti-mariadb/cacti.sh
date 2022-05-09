fa90e2669d4bff5fbfa1542e289e97f68e4da662
wutarnow <79676753+wutarnow@users.noreply.github.com>
2022-05-09 17:07:56 +0800

    Update cacti.sh

--- cacti-mariadb/cacti.sh
+++ cacti-mariadb/cacti.sh
@@ -1,9 +1,9 @@
 # Set database
 # sed -i '/\[mysqld\]/a character-set-server=utf8mb4 \
 # collation-server=utf8mb4_unicode_ci' /etc/my.cnf.d/mariadb-server.cnf
-mysql -e "CREATE DATABASE cacti;"
-mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql mysql
-mysql -e "ALTER DATABASE cacti CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-mysql -e "GRANT ALL ON cacti.* TO cactiuser@localhost IDENTIFIED BY 'cactiuser';"
-mysql -e "GRANT SELECT ON mysql.time_zone_name TO cactiuser@localhost;"
-mysql -e "FLUSH privileges;"
+mysql -u root -proot -e "CREATE DATABASE cacti;"
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -proot mysql
+mysql -u root -proot -e "ALTER DATABASE cacti CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+mysql -u root -proot -e "GRANT ALL ON cacti.* TO cactiuser@localhost IDENTIFIED BY 'cactiuser';"
+mysql -u root -proot -e "GRANT SELECT ON mysql.time_zone_name TO cactiuser@localhost;"
+mysql -u root -proot -e "FLUSH privileges;"
