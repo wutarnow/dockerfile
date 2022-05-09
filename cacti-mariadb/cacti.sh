1dbac4efad233793fd9693672b1efb1b778821ee
wutarnow <79676753+wutarnow@users.noreply.github.com>
2022-05-09 16:04:52 +0800

    Create cacti.sh

---
+++ cacti-mariadb/cacti.sh
@@ -0,0 +1,12 @@
+# Set the host timezone
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+  
+# Set database
+sed -i '/\[mysqld\]/a character-set-server=utf8mb4 \
+collation-server=utf8mb4_unicode_ci' /etc/my.cnf.d/mariadb-server.cnf
+mysql -e "CREATE DATABASE cacti;"
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql mysql
+mysql -e "ALTER DATABASE cacti CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+mysql -e "GRANT ALL ON cacti.* TO cactiuser@localhost IDENTIFIED BY 'cactiuser';"
+mysql -e "GRANT SELECT ON mysql.time_zone_name TO cactiuser@localhost;"
+mysql -e "FLUSH privileges;"
