e79275c7730491fc1a6be573f4824bb3aae2fc63
wutarnow <79676753+wutarnow@users.noreply.github.com>
2022-05-09 16:41:36 +0800

    Update cacti.sh

--- cacti-mariadb/cacti.sh
+++ cacti-mariadb/cacti.sh
@@ -2,8 +2,8 @@
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
   
 # Set database
-sed -i '/\[mysqld\]/a character-set-server=utf8mb4 \
-collation-server=utf8mb4_unicode_ci' /etc/my.cnf.d/mariadb-server.cnf
+# sed -i '/\[mysqld\]/a character-set-server=utf8mb4 \
+# collation-server=utf8mb4_unicode_ci' /etc/my.cnf.d/mariadb-server.cnf
 mysql -e "CREATE DATABASE cacti;"
 mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql mysql
 mysql -e "ALTER DATABASE cacti CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
